16441f49ec2f919ceb516385dd22411e3e2187e6
Stian Grindvoll <stian@grindvoll.org>
2018-06-27 14:09:23 +0200

    Add .rio to each message filename (#21)
    * use .rio postfix in messages inside tarball, it's difficult to handle these message on disk with random uuid alone.
    
    * We are using the header.Xattrs option now

--- file/tarball.go
+++ file/tarball.go
@@ -115,7 +115,7 @@ func UnPack(wg *sync.WaitGroup, file afero.File, messages chan rmq.Message) (n i
 		}
 
 		// generate and push the message to the output channel
-		messages <- *rmq.NewMessage(buf.Bytes(), hdr.PAXRecords)
+		messages <- *rmq.NewMessage(buf.Bytes(), hdr.Xattrs)
 		n++
 	}
 	return n, err
@@ -155,7 +155,7 @@ func (t *TarballBuilder) Pack(messages chan rmq.Message, dir string, verify chan
 			docNum = 0
 		}
 
-		if err := t.addFile(t.tar, uuid.New(), &doc); err != nil {
+		if err := t.addFile(t.tar, uuid.New()+".rio", &doc); err != nil {
 			return err
 		}
 	}
